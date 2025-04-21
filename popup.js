1217b65498990118ac6a5a599e0e6ea4b5e8c348
Asitha Navaratne <asithan476@gmail.com>
2025-04-21 23:12:06 +0530

    Added time to analog clock

--- popup.js
+++ popup.js
@@ -1,10 +1,6 @@
-function setInitialTimes() {
-  const localTime = new Date();
-  const hours = String(localTime.getHours()).padStart(2, "0");
-  const minutes = String(localTime.getMinutes()).padStart(2, "0");
+const localTime = new Date();
 
-  const timeString = `${hours}:${minutes}`;
-
+function setInitialUkTimes() {
   const ukTime = localTime.toLocaleTimeString("en-GB", {
     hour: "2-digit",
     minute: "2-digit",
@@ -14,14 +10,29 @@ function setInitialTimes() {
 
   document.querySelector(".clock-body__time").innerHTML = ukTime;
 
-  return timeString;
+  const [ukHour, ukMinutes] = ukTime.split(":");
+
+  const hourDegrees = ukHour * 30 + 270 + ukMinutes * 0.5;
+  const minuteDegrees = ukMinutes * 6 + 270;
+
+  document.querySelector(
+    ".clock-body__hour-hand"
+  ).style.transform = `rotate(${hourDegrees}deg)`;
+  document.querySelector(
+    ".clock-body__minute-hand"
+  ).style.transform = `rotate(${minuteDegrees}deg)`;
 }
 
-function setTimeInputValue(timeString) {
+function setInitialTimeInputValue() {
+  const hours = String(localTime.getHours()).padStart(2, "0");
+  const minutes = String(localTime.getMinutes()).padStart(2, "0");
+
+  const timeString = `${hours}:${minutes}`;
+
   document.querySelector(".time-input-section__input").value = timeString;
 }
 
-const timeString = setInitialTimes();
-setTimeInputValue(timeString);
+setInitialUkTimes();
+setInitialTimeInputValue();
 
-setInterval(setInitialTimes, 1000);
+setInterval(setInitialUkTimes, 1000);
