3823af3a430f6528f8f99ca556843f8a6aa8b1bd
Asitha Navaratne <asithan476@gmail.com>
2025-04-21 22:37:20 +0530

    Setting UK time

--- popup.js
+++ popup.js
@@ -1,10 +1,18 @@
-function setInitialTime() {
-  const hours = new Date().getHours();
-  const minutes = new Date().getMinutes();
+function setInitialTimes() {
+  const localTime = new Date();
+  const hours = String(localTime.getHours()).padStart(2, "0");
+  const minutes = String(localTime.getMinutes()).padStart(2, "0");
 
-  const timeString = `${hours}:${minutes >= 10 ? minutes : "0" + minutes}`;
+  const timeString = `${hours}:${minutes}`;
 
-  document.querySelector(".clock-body__time").innerHTML = timeString;
+  const ukTime = localTime.toLocaleTimeString("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+    timeZone: "Europe/London",
+  });
+
+  document.querySelector(".clock-body__time").innerHTML = ukTime;
 
   return timeString;
 }
@@ -13,7 +21,7 @@ function setTimeInputValue(timeString) {
   document.querySelector(".time-input-section__input").value = timeString;
 }
 
-const timeString = setInitialTime();
+const timeString = setInitialTimes();
 setTimeInputValue(timeString);
 
-setInterval(setInitialTime, 1000);
+setInterval(setInitialTimes, 1000);
